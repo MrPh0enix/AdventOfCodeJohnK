36a006d42413591da1bb579c41a1d757cd4f4916
JohnK <johnkiran02@gmail.com>
2025-12-11 23:33:35 +0000

    day8 part 1 2025

--- 2025/8/day8_1.py
+++ 2025/8/day8_1.py
@@ -23,10 +23,60 @@ for i in range(n):
         d = dist(points[i], points[j])
         heapq.heappush(heap, (d, i, j))
 
-result = []
+
+
+class UnionFind:
+
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        
+        return self.parent[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX == rootY:
+            return False
+        
+        if self.size[rootX] < self.size[rootY]:
+            rootX, rootY = rootY, rootX
+        
+        self.parent[rootY] = rootX
+        self.size[rootX] += self.size[rootY]
+        return True
+
+    
+    def group_size(self, x):
+        return self.size[self.find(x)]
+
+
+
+
+uf = UnionFind(len(points))
+
+
+
 for i in range(1000):
     d, i, j = heapq.heappop(heap)
-    for 
+    
+    uf.union(i, j)
+
+
+largets_sizes = sorted([uf.size[i] for i in range(len(uf.parent)) if uf.parent[i] == i], reverse = True)[0:3]
+
+out = 1
+for i in largets_sizes:
+    out *= i
+
+print(out)
+
+
 
 
 
